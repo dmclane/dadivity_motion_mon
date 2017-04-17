f24ef39f822b88c8496e104aac39ca9be29297db
Don McLane <dmclane@ieee.org>
2017-04-16 20:53:43 -0500

    Added hardware pin numbers to pi_resources.py

--- pi_resources.py
+++ pi_resources.py
@@ -1,8 +1,8 @@
 
 # keep hardware definitions for the Pi all in one place
-MOTION_SENSOR_PIN = 17
-BUTTON_PIN = 27
-BEEPER_PIN_1 = 22
-BEEPER_PIN_2 = 23
+MOTION_SENSOR_PIN = 17        # GPIO17 ==> pin 11
+BUTTON_PIN = 27               # GPIO27 ==> pin 13
+BEEPER_PIN_1 = 22             # GPIO22 ==> pin 15
+BEEPER_PIN_2 = 23             # GPIO23 ==> pin 16
 # MAIL_TRIGGER_PIN = 24
 
